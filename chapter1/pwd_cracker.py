17f8696754baab5899f3a63525d6300b407fb712
x14119641 <daniel.gil.romero24@gmail.com>
2020-01-02 22:06:15 +0000

    Need to use linus for crypt

--- chapter1/pwd_cracker.py
+++ chapter1/pwd_cracker.py
@@ -2,7 +2,11 @@ import crypt
 
 def test_pass(crypt_pass):
     salt = crypt_pass[0:2]
-    with open('CH1/passwords.txt', 'r') as f:
+    with open(r'chapter1\CH1\dictionary.txt') as f:
         for word in f.readlines():
             print(word)
             word = word.strip('\n')
+
+
+if __name__=='__main__':
+    test_pass([])
